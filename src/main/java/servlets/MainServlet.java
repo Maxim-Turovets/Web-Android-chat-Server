95ae2cc52dbf6372c85310c100774eaf99ed8eb3
Maxim-Turovets <mturove71@gmail.com>
2019-03-03 18:38:11 +0200

    add test 2 to server

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Test");
+        resp.getWriter().write("Test 2");
     }
 }
