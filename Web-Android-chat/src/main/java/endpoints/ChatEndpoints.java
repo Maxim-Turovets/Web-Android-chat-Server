53518f0e19eabf79aa43757346b84d9937a5f63b
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2019-03-09 12:25:04 +0200

    add new  function

--- Web-Android-chat/src/main/java/endpoints/ChatEndpoints.java
+++ Web-Android-chat/src/main/java/endpoints/ChatEndpoints.java
@@ -3,31 +3,37 @@ package endpoints;
         import coders.MessageDecoder;
         import coders.MessageEncoder;
         import entities.Message;
+        import room.Room;
 
         import javax.websocket.*;
         import javax.websocket.server.ServerEndpoint;
         import java.io.IOException;
+        import java.util.ArrayList;
         import java.util.LinkedList;
         import java.util.List;
 
 @ServerEndpoint(value = "/chat",decoders = {MessageDecoder.class},encoders = {MessageEncoder.class})
 public class ChatEndpoints {
     private Session session=null;
-    public  static int counter = 0;
-    private  static List<Session> sessionList = new LinkedList<>();
+    private  static List<Session> generalChatSessionList = new LinkedList<>();
+    private  static List<Session> pairChatSessionList = new ArrayList<>();
+    private  static List<Room>    roomList = new ArrayList<>();
+    public  int index= roomList.size()-1;
+    public boolean isConnected=false;
+
 
     @OnOpen
     public  void onOpen (Session session)
     {
-        counter++;
         this.session = session;
-        sessionList.add(session);
+        isConnected =true;
     }
 
     @OnClose
     public  void onClose(Session session)
     {
-        sessionList.remove(this.session);
+        generalChatSessionList.remove(this.session);
+        roomList.remove(index);
     }
     @OnError
     public  void onError (Session session,Throwable throwable)
@@ -35,37 +41,121 @@ public class ChatEndpoints {
         throwable.printStackTrace();
     }
 
-//    @OnMessage
-//    public  void  onMessage(Session session, Message msg)
-//    {
-//        sessionList.forEach(s->{
-//            if(s==this.session) return;
-//            try {
-//                s.getBasicRemote().sendObject(msg);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } catch (EncodeException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//    }
-
     @OnMessage
-    public  void  onMessage(Session session,String str) throws IOException {
+    public  void  onMessage(Session session, Message msg)
+    {
 
-        session.getBasicRemote().sendText(str);
-        for(int i = 0;i<sessionList.size();i++)
-        {
-            if(sessionList.get(i)==session)
-            continue;
-            else
-                sessionList.get(i).getBasicRemote().sendText(str);
+        if(isConnected==true) {
+            System.out.println("Client "+session.getId());
+            if (setChatType(msg).equals("general")) {
+                generalChatSessionList.add(this.session);
+                isConnected = false;
+            }
+            if (setChatType(msg).equals("pair")) {
+                pairChatSessionList.add(this.session);
+                setClientToRoom();
+                isConnected= false;
+            }
         }
+
+            if (setChatType(msg).equals("general")) {
+                sendMessagesGeneralChat(msg);
+            }
+            if (setChatType(msg).equals("pair")) {
+                sendMessagesRoomChat(msg);
+            }
+
+    }
+
+
+private void sendMessagesGeneralChat(Message message) // разослать в общем чате
+{
+    generalChatSessionList.forEach(s->{
+        if(s==this.session) return;
+        try {
+            s.getBasicRemote().sendObject(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (EncodeException e) {
+            e.printStackTrace();
+        }
+    });
+}
+
+private  String  setChatType(Message messageClient)
+{
+    if (messageClient.getAuthkey().equals("pair"))
+        return "pair";
+    if(messageClient.getAuthkey().equals("general"))
+        return "general";
+    else
+        return "";
+}
+
+private void setClientToRoom()
+{
+    boolean y = false;
+
+    for(int i=0;i<roomList.size();i++)
+    {
+        if (roomList.get(i).getStatus()==true) {
+            System.out.println("Room close");
+            roomList.get(i).addElement(this.session);
+            y = true;
+            roomList.get(i).setStatus(false);
+            break;
+        }
+    }
+    if (y==false)
+    {
+            Room room = new Room();
+            room.addElement(this.session);
+            room.setStatus(true);
+            roomList.add(room);
+            index = roomList.size()-1;
+        System.out.println("Room create " + index);
     }
 
 
 
+      pairChatSessionList.remove(0);
 
 
 
+}
+
+    private void sendMessagesRoomChat(Message message)    // разослать в room
+    {
+        String msg = message.getName();
+        message.setName("(private room) " + msg);
+        System.out.println("Index " + index);
+
+        try {
+            if (this.session == roomList.get(index).getClientList().get(0)) {
+                System.out.println("1 Session");
+                try {
+                    roomList.get(index).getClientList().get(1).getBasicRemote().sendObject(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (this.session == roomList.get(index).getClientList().get(1)) {
+                System.out.println("2 Session");
+                try {
+                    roomList.get(index).getClientList().get(0).getBasicRemote().sendObject(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.err.println("To client not create");
+        }
+
+    }
 }
