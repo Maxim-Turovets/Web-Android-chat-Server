1ec99ecde3727c5d6e96b69d1b5cae570f65226e
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2019-03-17 19:22:56 +0200

    Update ChatEndpoints.java

--- Web-Android-chat/src/main/java/endpoints/ChatEndpoints.java
+++ Web-Android-chat/src/main/java/endpoints/ChatEndpoints.java
@@ -1,19 +1,19 @@
 package endpoints;
 
-        import coders.MessageDecoder;
-        import coders.MessageEncoder;
+        import clientObject.ConnectInfo;
+        import clientObject.UserInfo;
         import entities.Message;
         import room.Room;
 
         import javax.websocket.*;
         import javax.websocket.server.ServerEndpoint;
         import java.io.IOException;
-        import java.lang.reflect.InvocationTargetException;
         import java.util.ArrayList;
         import java.util.LinkedList;
         import java.util.List;
+        import objecttype.ObjectType;
 
-@ServerEndpoint(value = "/chat",decoders = {MessageDecoder.class},encoders = {MessageEncoder.class})
+@ServerEndpoint(value = "/chat"/*,decoders = {MessageDecoder.class},encoders = {MessageEncoder.class}*/)
 public class ChatEndpoints {
     private Session session=null;
     private  static List<Session> generalChatSessionList = new LinkedList<>();
@@ -46,33 +46,38 @@ public class ChatEndpoints {
     @OnError
     public  void onError (Session session,Throwable throwable)
     {
-        System.err.println("Session close");
+        //System.err.println("Session close");
+        System.out.println(throwable);
     }
 
     @OnMessage
-    public  void  onMessage(Session session, Message msg)
+    public  void  onMessage(Session session, String stringJson)
     {
+        System.out.println(stringJson);
 
-        if(isConnected==true)
-        {
-            System.out.println("Client "+session.getId());
-            if (setChatType(msg).equals("general")) {
-                generalChatSessionList.add(this.session);
-                isConnected = false;
-            }
-            if (setChatType(msg).equals("pair")) {
-                pairChatSessionList.add(this.session);
-                setClientToRoom();
-                isConnected= false;
-            }
+        System.out.println(objectInfo(stringJson));
+
+        if (objectInfo(stringJson).toString().equals("UserInfo")) {
+            UserInfo userInfo = new UserInfo();
+            userInfo = (UserInfo) ObjectType.getObject(stringJson, userInfo);
+            System.err.println("Name : "+userInfo.getName());
+            System.err.println("Gender : "+userInfo.getGender());
+            System.err.println("Age : "+userInfo.getAge());
+            System.err.println("Voice : "+userInfo.isVoiceMessage());
         }
 
-            if (setChatType(msg).equals("general")) {
-                sendMessagesGeneralChat(msg);
-            }
-            if (setChatType(msg).equals("pair")) {
-                sendMessagesRoomChat(msg);
-            }
+        if(objectInfo(stringJson).toString().equals("ConnectInfo")) {
+            ConnectInfo connectInfo = new ConnectInfo();
+            connectInfo = (ConnectInfo) ObjectType.getObject(stringJson, connectInfo);
+            System.err.println("Chat type : "+connectInfo.getChatType());
+        }
+
+
+
+
+
+
+
 
     }
 
@@ -82,89 +87,28 @@ private void sendMessagesGeneralChat(Message message) // разослать в �
     generalChatSessionList.forEach(s->{
         if(s==this.session) return;
         try {
-            s.getBasicRemote().sendObject(message);
+            s.getBasicRemote().sendText(ObjectType.getJson(message));
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (EncodeException e) {
             e.printStackTrace();
         }
     });
 }
 
-private  String  setChatType(Message messageClient)
-{
-    if (messageClient.getAuthkey().equals("pair"))
-        return "pair";
-    if(messageClient.getAuthkey().equals("general"))
-        return "general";
-    else
-        return "";
-}
 
-private void setClientToRoom()
-{
-    boolean y = false;
-
-    for(int i=0;i<roomList.size();i++)
-    {
-        if (roomList.get(i).getStatus()==true) {
-            System.out.println("Room close");
-            roomList.get(i).addElement(this.session);
-            y = true;
-            roomList.get(i).setStatus(false);
-            break;
-        }
-    }
-    if (y==false)
-    {
-            Room room = new Room();
-            room.addElement(this.session);
-            room.setStatus(true);
-            roomList.add(room);
-            index = roomList.size()-1;
-            System.out.println("Room create " + index);
-    }
-
-
-
-      pairChatSessionList.remove(0);
-
-
-
-}
-
-    private void sendMessagesRoomChat(Message message)    // разослать в room
-    {
-        String msg = message.getName();
-        message.setName("(private room) " + msg);
-        System.out.println("Index " + index);
-
-        try {
-            if (this.session == roomList.get(index).getClientList().get(0)) {
-                System.out.println("1 Session");
-                try {
-                    roomList.get(index).getClientList().get(1).getBasicRemote().sendObject(message);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (EncodeException e) {
-                    e.printStackTrace();
-                }
+    private StringBuffer objectInfo(String json){
+        StringBuffer returnJson = new StringBuffer();
+        int count =0;
+        for(int i=0;i<json.length();i++)
+        {
+            if(json.charAt(i)=='\"')
+            {
+                count++;
             }
-
-            if (this.session == roomList.get(index).getClientList().get(1)) {
-                System.out.println("2 Session");
-                try {
-                    roomList.get(index).getClientList().get(0).getBasicRemote().sendObject(message);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (EncodeException e) {
-                    e.printStackTrace();
-                }
+            if (count==3){
+                returnJson.append(json.charAt(i));
             }
         }
-        catch (IndexOutOfBoundsException e) {
-            System.err.println("To client not create");
-        }
-
+        returnJson.deleteCharAt(0);
+        return returnJson;
     }
 }
